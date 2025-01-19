b241aff6b88b2d4639e1aabbbcb91c9a6fe39913
potakaaa <helbirog@gmail.com>
2025-01-19 18:37:51 +0800

    setup(backend and frontend): individual setup done, integration is missing

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173/']
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
