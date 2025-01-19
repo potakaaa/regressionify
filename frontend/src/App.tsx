4c7581760aaef3c97a6fb47419304dd5ced53092
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-19 19:16:07 +0800

    connected front and back, added requirements.txt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const testApi = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/");
+    if (response.ok) console.log("api is connecting");
+  };
+
+  useEffect(() => {
+    testApi();
+  }, []);
 
   return (
     <>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
