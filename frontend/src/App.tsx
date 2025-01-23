759a1fbdc854b0ffaef90f65521558645483d96a
potakaaa <helbirog@gmail.com>
2025-01-23 23:46:29 +0800

    (frontend): added hidden mode toggle on app and customized css themes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "./components/theme/mode-toggle";
 import Homepage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <div className="w-full h-screen max-h-screen flex justify-center items-center bg-background">
+    <div className="w-full h-screen max-h-screen flex flex-col justify-center items-center bg-background">
+      {/* <ModeToggle /> */}
       <Homepage />
     </div>
   );
