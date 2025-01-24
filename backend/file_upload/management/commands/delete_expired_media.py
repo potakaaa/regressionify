fc05841da788261597181319c30966785d8ca015
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-24 10:18:50 +0800

    implemented expired files script

--- backend/file_upload/management/commands/delete_expired_media.py
+++ backend/file_upload/management/commands/delete_expired_media.py
@@ -18,6 +18,10 @@ def delete_expired_media():
 
     return f"{expired_media_files.count()} expired media files have been deleted."
 
+
+# i will implement this as a cron job on deployment. Does not make sense to run this on development
+# for test purposes, u may simply run "manage.py delete_expired_media" to see the output,
+# it will delete all expired media files (older than an hour) from the database and the filesystem
 class Command(BaseCommand):
     help = "Deletes expired media files"
 
