d15f0105b2dfd92c753cb75415291ce4357cc4c5
potakaaa <helbirog@gmail.com>
2025-01-28 20:38:02 +0800

    (frontend): adding cards of added independent variables

--- frontend/src/components/IndependentCard.tsx
+++ frontend/src/components/IndependentCard.tsx
@@ -13,6 +13,7 @@ import { Button } from "@/components/ui/button";
 import { useGlobalState } from "./provider/global-provider";
 import { FC } from "react";
 import DropDown, { DropDownProps } from "./Dropdown";
+import { Trash } from "lucide-react";
 
 const independent = ["X1", "X2", "X3"];
 
@@ -38,7 +39,22 @@ const IndependentCard = () => {
           <AlertDialogTitle>Select Independent Variables</AlertDialogTitle>
           <AlertDialogDescription>
             {/* HAVE TO MAP INDEPENDENTS HERE AND CREATE LAYOUR FOR ADDED INDEPENDENTS */}
-            <DropDown propList={independent}></DropDown>
+
+            <div className="flex flex-row justify-between">
+              <div className="grid grid-cols-3 gap-3">
+                {independent.map((item) => (
+                  <div className="flex flex-row p-2 items-center justify-between border border-secondary rounded-lg w-24">
+                    <p className=" text-ellipsis inline-block overflow-hidden">
+                      {item}
+                    </p>
+                    <button className="hover:bg-white hover:bg-opacity-10 p-1 rounded-lg">
+                      <Trash size={15} className="text-red-500" />
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <DropDown propList={independent}></DropDown>
+            </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
