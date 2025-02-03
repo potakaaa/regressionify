579282d39493b010450c5d3fcfcee5ac2c541f35
potakaaa <helbirog@gmail.com>
2025-02-03 22:13:56 +0800

    added framer motion on homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,6 +4,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-dropdown-menu";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { motion, stagger } from "framer-motion";
+import { formElementMotion, formMotion } from "@/utils/motionVariants";
 
 const sheetNames = ["Sheet1", "Sheet2", "Sheet3"];
 const dependent = ["Y1", "Y2", "Y3"];
@@ -79,46 +81,63 @@ const HomePage = () => {
     >
       <div
         id="title-container"
-        className="flex flex-col justify-center items-center"
+        className="flex flex-col justify-center items-center mb-10"
       >
         <h1 className="font-extrabold text-4xl text-primary">REGRESSIONIFY</h1>
         <p className="font-medium w-72 px-2 text-sm text-center">
           A Statistical Tool for Mathematicians and Statisticians.
         </p>
       </div>
-      <div
+      <motion.section
         id="form-container"
         className="space-y-3 w-72 flex flex-col justify-center"
+        variants={formMotion}
+        initial="hidden"
+        animate="show"
       >
-        <h1 className="font-extrabold text-xl text-primary">
+        <motion.h1
+          variants={formElementMotion}
+          className="font-extrabold text-xl text-primary"
+        >
           Worksheet Details
-        </h1>
-        <div
+        </motion.h1>
+        <motion.div
           id="sheet-name-container"
           className="w-full flex flex-row justify-between items-center"
+          variants={formElementMotion}
         >
           <p className="text-sm w-32 font-medium">Reference Sheet Name</p>
           <DropDown propList={sheetNames} />
-        </div>
-        <div
+        </motion.div>
+        <motion.div
+          variants={formElementMotion}
           id="dependent-var-container"
           className="w-full flex flex-row justify-between items-center"
         >
           <p className="text-sm w-32 font-medium">Dependent Variable</p>
           <DropDown propList={dependent} />
-        </div>
-        <div
+        </motion.div>
+        <motion.div
+          variants={formElementMotion}
           id="independent-var-container"
           className="w-full flex flex-row justify-between items-center"
         >
           <p className="text-sm w-32 font-medium">Independent Variable</p>
           <IndependentCard />
-        </div>
-        <div id="file-upload-container" className="w-full pt-7 space-y-1">
+        </motion.div>
+        <motion.div
+          variants={formElementMotion}
+          id="file-upload-container"
+          className="w-full pt-7 space-y-1"
+        >
           <Label className="text-sm font-medium ml-1">Upload Excel File</Label>
-          <Input type="file" accept=".xlsx, .xls" className="text-sm" />
-        </div>
-      </div>
+          <Input
+            type="file"
+            accept=".xlsx, .xls"
+            className="text-sm hover:cursor-pointer hover:bg-zinc-800 duration-300"
+          />
+        </motion.div>
+      </motion.section>
     </div>
   );
 };
